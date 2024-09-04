2c03e1bd888e41fb38857d68ceb7bec0b3edba43
AbiZaid855 <jorgeabizaid@gmail.com>
2024-09-04 17:14:48 -0300

    teste 06

--- src/App.java
+++ src/App.java
@@ -1,10 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World Hello, World");
-        System.out.println("Hello, World Hello, World");
-        System.out.println("Hello, World Hello, World");
-        System.out.println("Hello, World Hello, World");
-        System.out.println("Hello, World Hello, World");
+        System.out.println("Hello, World");
+
         
     }
 }
