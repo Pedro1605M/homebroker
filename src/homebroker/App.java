62a849a1d4100ff24b0384e267070139afcd8c85
Pedro1605M <pedrocosta201207@gmail.com>
2024-09-14 10:41:19 -0300

    errei

--- src/homebroker/App.java
+++ src/homebroker/App.java
@@ -1,8 +1,8 @@
-public class App {
-    public static void main(String[] args) throws Exception {
+public class App{
+    public static void main(String[] args) throws Exception{
         System.out.println("homebroker");
         System.out.println("Teste1");
-
-        
     }
 }
+
+
