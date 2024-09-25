8ddd259395b4bafdef21bcf1f88cf0d17e46657a
Pedro1605M <pedrocosta201207@gmail.com>
2024-09-25 17:01:12 -0300

    criação da classe abstrata pessoa

---
+++ src/homebroker/Pessoa.java
@@ -0,0 +1,62 @@
+import java.sql.Date;
+
+public abstract class Pessoa {
+    
+    private String nome;
+    private Date nascimento;
+    private String cpf;
+    private String email;
+    private String senha;
+    
+    public Pessoa(String nome, Date nascimento, String cpf, String email, String senha) {
+        this.nome = nome;
+        this.nascimento = nascimento;
+        this.cpf = cpf;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(Date nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    
+    
+
+}
