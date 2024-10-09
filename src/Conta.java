729e78b2182312145b0706301b2d0e1b6088f9d6
Pedro1605M <pedrocosta201207@gmail.com>
2024-10-09 16:45:53 -0300

    ajustando as classes

--- src/Conta.java
+++ src/Conta.java
@@ -5,14 +5,12 @@ public class Conta{
     private Pessoa conta;
     private Double saldo;
     private Integer numeroDaConta;
-    private Modalidade deposito;
     ArrayList<Acoes> acoes = new ArrayList();
     
-    public Conta(Pessoa conta, Double saldo, Integer numeroDaConta, Modalidade deposito, ArrayList<Acoes> acoes) {
+    public Conta(Pessoa conta, Double saldo, Integer numeroDaConta, ArrayList<Acoes> acoes) {
         this.conta = conta;
         this.saldo = saldo;
         this.numeroDaConta = numeroDaConta;
-        this.deposito = deposito;
         this.acoes = acoes;
     }
 
@@ -39,15 +37,7 @@ public class Conta{
     public void setNumeroDaConta(Integer numeroDaConta) {
         this.numeroDaConta = numeroDaConta;
     }
-
-    public Modalidade getDeposito() {
-        return deposito;
-    }
-
-    public void setDeposito(Modalidade deposito) {
-        this.deposito = deposito;
-    }
-
+    
     public ArrayList<Acoes> getAcoes() {
         return acoes;
     }
--- src/ControllerCadastro.java
+++ src/ControllerCadastro.java
@@ -25,7 +25,7 @@ public class ControllerCadastro {
 
     @FXML
     void cadastrar(ActionEvent event) {
-
+        
     }
 
     @FXML
